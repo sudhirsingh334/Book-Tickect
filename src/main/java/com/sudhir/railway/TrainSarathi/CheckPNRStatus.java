9f60c4d232d7784c3b05abcf2ecdc9a2950cd2d7
Sudhir Kumar Singh <sudhir.manojsingh@gmail.com>
2023-03-19 19:45:16 +0530

    PS-2: printing details in table

--- src/main/java/com/sudhir/railway/TrainSarathi/CheckPNRStatus.java
+++ src/main/java/com/sudhir/railway/TrainSarathi/CheckPNRStatus.java
@@ -6,9 +6,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.Timestamp;    
+import java.util.Date;  
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sudhir.railway.pojo.*;
+import com.sudhir.railway.pojo.PNRStatusResponse;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -27,8 +29,8 @@ public class CheckPNRStatus extends HttpServlet {
 
 		String baseURLString = "https://irctc1.p.rapidapi.com/api/v3/getPNRStatus?pnrNumber=";
 
-		 String urlString = baseURLString+pnrNumber;
-		//String urlString = "https://869849af-dec5-46c9-bcd3-c36e5b08cc0e.mock.pstmn.io/getPNRStatus";
+		 //String urlString = baseURLString+pnrNumber;
+		String urlString = "https://869849af-dec5-46c9-bcd3-c36e5b08cc0e.mock.pstmn.io/getPNRStatus";
 
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString))
 				.header("X-RapidAPI-Key", "84ec970e55msh29237cd1c10520dp1cc193jsn773b967a76d6")
@@ -43,8 +45,7 @@ public class CheckPNRStatus extends HttpServlet {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println(response.body());
-
+		
 		String jsonString = response.body();
 
 		ObjectMapper objectMapper = new ObjectMapper();
@@ -57,15 +58,49 @@ public class CheckPNRStatus extends HttpServlet {
 			return;
 		}
 		
-		out.println("Message: " + pnrResponse.message);
-		out.println("Arrival Time: " + pnrResponse.pnrDetails.arrivalTime);
-		out.println("Coach Position: " + pnrResponse.pnrDetails.coachPosition);
-				
+		Timestamp ts=new Timestamp(System.currentTimeMillis());  
+        Date date=new Date(ts.getTime());  
+        
+        out.print("<br>");
+		out.println(date);
+		out.print("<br><br>");
+		
+		
+		out.println("<table border='1'> <tr> <th>Train Number</th> <th>Train Name</th> <th>Boarding Date <br>"
+				+ "(DD-MM-YYYY)</th><th>From</th><th>Reserved Upto</th><th>Boarding Point</th><th>Class</th></tr> ");
+		  
+		out.print("<tr>");
+		out.print("<td>"+ pnrResponse.pnrDetails.trainNo +"</td>");
+		out.print("<td>"+ pnrResponse.pnrDetails.trainName +"</td>");		
+		out.print("<td>"+ pnrResponse.pnrDetails.sourceDoj +"</td>");		
+		out.print("<td>"+ pnrResponse.pnrDetails.from +"</td>");		
+		out.print("<td>"+ pnrResponse.pnrDetails.to +"</td>");	
+		out.print("<td>"+ pnrResponse.pnrDetails.reservationUpto +"</td>");
+		out.print("<td>"+ pnrResponse.pnrDetails.boardingPoint +"</td>");
+		out.print("<td>"+ pnrResponse.pnrDetails.bookedClass +"</td>");
+		out.print("</tr>");
+		out.print("</table>");
+
+		out.print("<table border='1'> <tr> <th>S.No.</th> <th>Booking Status (Coach No , Berth No., Quota)</th> <th>*Current Status (Coach No , Berth No.)</th><th>Coach Position</th><br></tr>");
+		
 		pnrResponse.pnrDetails.passengerList.forEach((passenger -> {
-			out.println("######################################");
-			out.println("Status: " + passenger.bookingStatus);
-			out.println("Berth: " + passenger.berth);
-			out.println("Coach: " + passenger.coach);
+			out.print("<tr>");
+			out.print("<td>"+ "Passenger 1" +"</td>");
+			out.print("<td>"+ passenger.bookingStatus +"</td>");		
+			out.print("<td>"+ passenger.currentStatus +"</td>");		
+			out.print("<td>"+ " " +"</td>");		
+			out.print("</tr>");
 		})) ;
+		
+		out.print("</table>");
+		
+		out.println("<br><table border='1'> <tr> <th>Total Fare</th> <th>Charting Status</th> <th>Remarks if any</th><th>Train Status</th></tr>");
+
+		out.print("<tr>");
+		out.print("<td>"+ pnrResponse.pnrDetails.bookingFare +"</td>");
+		out.print("<td>"+ (pnrResponse.pnrDetails.chartPrepared ? "Chart Prepared":"Chart Not Prepared") +"</td>");		
+		out.print("<td> </td>");		
+		out.print("<td>"+ pnrResponse.pnrDetails.trainStatus +"</td>");				
+		out.print("</table>");
 	}
 }
